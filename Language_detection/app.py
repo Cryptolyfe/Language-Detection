bd4f557f47f0e360bcd85dfac25e1315e82256cc
Katie Kirby <jackalhandz@protonmail.com>
2023-11-12 20:17:39 -0800

    Update app.py

--- Language_detection/app.py
+++ Language_detection/app.py
@@ -2,18 +2,28 @@ import pickle
 import string
 import streamlit as st
 import webbrowser
-
+import os
 global Lrdetect_Model
 
-LrdetectFile = open('/Users/katie.kirby/projects/language_detection/Language_detection/model.pckl', 'rb')
 
 
-#LrdetectFile = open('model.pckl','rb')
-Lrdetect_Model = pickle.load(LrdetectFile)
-LrdetectFile.close()
+
+# Get the full path to 'model.pckl'
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model.pckl')
+
+# Open the 'model.pckl' file using the full path
+try:
+    LrdetectFile = open(model_path, 'rb')
+    Lrdetect_Model = pickle.load(LrdetectFile)
+    LrdetectFile.close()
+except FileNotFoundError:
+    st.error(f"Error: 'model.pckl' file not found at {model_path}. Make sure the file exists in the correct location.")
+    st.stop()
+
 st.title("Language Detection Tool")
-input_test = st.text_input("provide your text input here", 'Hello my name is Katie ')
+input_test = st.text_input("Provide your text input here", 'Hello my name is Katie')
 
 button_clicked = st.button("Get Language Name")
 if button_clicked:
-	st.text(Lrdetect_Model.predict([input_test]))
+    st.text(Lrdetect_Model.predict([input_test]))
+
